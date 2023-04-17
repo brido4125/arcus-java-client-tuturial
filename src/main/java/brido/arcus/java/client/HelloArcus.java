b83f43c37ee5cbaa753ed2669f00c44f77a7545f
brido4125 <hcs4125@gmail.com>
2023-04-17 18:20:16 +0900

    test:BOP 테스트 코드 작성

--- src/main/java/brido/arcus/java/client/HelloArcus.java
+++ src/main/java/brido/arcus/java/client/HelloArcus.java
@@ -2,6 +2,11 @@ package brido.arcus.java.client;
 
 import net.spy.memcached.ArcusClient;
 import net.spy.memcached.ConnectionFactoryBuilder;
+import net.spy.memcached.collection.CollectionAttributes;
+import net.spy.memcached.collection.ElementValueType;
+import net.spy.memcached.internal.CollectionFuture;
+import net.spy.memcached.internal.OperationFuture;
+import net.spy.memcached.ops.StatusCode;
 
 import java.util.Objects;
 import java.util.concurrent.Future;
@@ -20,16 +25,15 @@ public class HelloArcus {
         this.arcusClient = ArcusClient.createArcusClient(arcusAdmin, serviceCode, new ConnectionFactoryBuilder());
     }
 
-    public boolean sayHello() {
-        Future<Boolean> future = null;
+    public boolean setHello(String key,Object value) {
+        OperationFuture<Boolean> future = null;
         boolean setSuccess = false;
 
         try {
-            future = this.arcusClient.set("test", 600, "Brido");
+            future = this.arcusClient.set(key, 6, value);
             setSuccess = future.get(700L, TimeUnit.MILLISECONDS);
             return setSuccess;
         } catch (Exception e) {
-            System.out.println("future = " + future);
             if (future != null)
                 future.cancel(true);
             e.printStackTrace();
@@ -37,10 +41,25 @@ public class HelloArcus {
         return false;
     }
 
-    public String listenHello() {
+    public boolean addData(String key,Object value) {
+        Future<Boolean> future = null;
+        boolean ret = false;
+        try {
+            future = this.arcusClient.add(key, 6, value);
+            ret = future.get(700L, TimeUnit.MILLISECONDS);
+            return ret;
+        } catch (Exception e) {
+            if (future != null)
+                future.cancel(true);
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public String getData(String key) {
         Future<Object> future = null;
         String res = "Not Yet";
-        future = this.arcusClient.asyncGet("test");
+        future = this.arcusClient.asyncGet(key);
         try {
             res = (String)future.get(700L, TimeUnit.MILLISECONDS);
         } catch (Exception e) {
@@ -48,5 +67,54 @@ public class HelloArcus {
             e.printStackTrace();
         }
         return res;
+    }
+
+    public boolean deleteData(String key) {
+        Future<Boolean> future = null;
+        boolean ret = false;
+        try {
+            future = this.arcusClient.delete(key);
+            ret = future.get(700L, TimeUnit.MILLISECONDS);
+            return ret;
+        } catch (Exception e) {
+            if (future != null)
+                future.cancel(true);
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean createBop(String key) {
+        CollectionFuture<Boolean> future = null;
+        CollectionAttributes attribute = new CollectionAttributes(); // (1)
+        attribute.setExpireTime(6);
+        boolean ret = false;
+        try {
+            future = this.arcusClient.asyncBopCreate(key, ElementValueType.INTEGER, attribute);
+            ret = future.get();
+            return ret;
+        } catch (Exception e) {
+            if (future != null)
+                future.cancel(true);
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean insertBop(String key, long bKey, Object value) {
+        CollectionFuture<Boolean> future = null;
+        CollectionAttributes ca = new CollectionAttributes();
+        byte[] eflag = new byte[] { 1, 1, 1, 1 };
+        boolean ret = false;
+        try {
+            future = this.arcusClient.asyncBopInsert(key, bKey, eflag, value, ca);
+            ret = future.get();
+            return ret;
+        } catch (Exception e) {
+            if (future != null)
+                future.cancel(true);
+            e.printStackTrace();
+        }
+        return false;
     }
 }
--- src/test/java/brido/arcus/java/client/HelloArcusTest.java
+++ src/test/java/brido/arcus/java/client/HelloArcusTest.java
@@ -1,32 +1,72 @@
 package brido.arcus.java.client;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelloArcusTest {
 
     private HelloArcus helloArcus;
+    private final String FOO_VAL = "Brido";
+    private final String TEST_KEY = "test";
 
     @BeforeEach
     public void set() {
         helloArcus = new HelloArcus("127.0.0.1:2191", "brido");
     }
 
+    @AfterEach
+    public void after() {
+        helloArcus.deleteData(TEST_KEY);
+    }
+
     @Test
     public void store() {
-        boolean b = helloArcus.sayHello();
-        System.out.println("b = " + b);
+        boolean b = helloArcus.setHello(TEST_KEY,"Brido");
+        assertThat(b).isTrue();
     }
 
     @Test
     void listenHello() {
         //given
+        helloArcus.setHello(TEST_KEY,"Brido");
         //when
-        String s = helloArcus.listenHello();
+        String s = helloArcus.getData(TEST_KEY);
         //then
-        System.out.println("s = " + s);
+        assertThat(s).isEqualTo(FOO_VAL);
+    }
+
+    @Test
+    void valueTimeOutTest() throws InterruptedException {
+        helloArcus.setHello("test","Brido");
+        Thread.sleep(6000);
+        String s = helloArcus.getData(TEST_KEY);
+        assertThat(s).isEqualTo(null);
+    }
+
+    @Test
+    @DisplayName("Collection Get OP Test")
+    void collectionGetTest() {
+        helloArcus.createBop(TEST_KEY);
+        helloArcus.insertBop(TEST_KEY, 1L, "TEST-VALUE");
+        String s = helloArcus.getBop(TEST_KEY, 1L);
+        assertThat(s).isEqualTo("TEST-VALUE");
+    }
+
+    @Test
+    @DisplayName("중복키 저장 - 저장 실패")
+    void dupKeySave() {
+        helloArcus.setHello(TEST_KEY, "Brido");
+        boolean b = helloArcus.addData("test", "Brido");
+        assertThat(b).isFalse();
+    }
+
+    @Test
+    @DisplayName("BTree Collection 생성")
+    void createBop() {
+        helloArcus.createBop(TEST_KEY);
+        boolean b = helloArcus.insertBop(TEST_KEY, 1L, "TEST-VALUE");
+        assertThat(b).isTrue();
     }
 }
