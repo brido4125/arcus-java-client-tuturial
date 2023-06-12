acdb272c86bc3359393af76cb4c301532bfafd4a
brido4125 <hcs4125@gmail.com>
2023-06-12 14:38:54 +0900

    update:LOP Collection bulk 관련 테스트 코드

--- src/main/java/ArcusLop.java
+++ src/main/java/ArcusLop.java
@@ -1,2 +1,60 @@
-public class ArcusLop {
+import net.spy.memcached.collection.CollectionAttributes;
+import net.spy.memcached.internal.CollectionFuture;
+import net.spy.memcached.ops.CollectionOperationStatus;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class ArcusLop extends ArcusInfo{
+    public ArcusLop(String arcusAdmin, String serviceCode) {
+        super(arcusAdmin, serviceCode);
+    }
+
+
+    /*
+    * 하나의 Key를 가지는 하나의 List에 여러개의 요소를 Bulk성으로 Insertion
+    * */
+    public Map<Integer, CollectionOperationStatus> insertLopPipedBulk(String key, List<Object> elements, CollectionAttributes attributesForCreate) {
+        CollectionFuture<Map<Integer, CollectionOperationStatus>> future = null;
+        future = arcusClient.asyncLopPipedInsertBulk(key, 0, elements, attributesForCreate);
+        Map<Integer, CollectionOperationStatus> rv = null;
+        try {
+            rv = future.get(1000L, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            throw new IllegalStateException();
+        }
+        CollectionOperationStatus operationStatus = future.getOperationStatus();
+        System.out.println("operationStatus = " + operationStatus);
+        return rv;
+    }
+
+
+    /*
+    * 여러개의 Key을 인수로 받아, 여러개의 List에 한개의 요소를 Insertion
+    * */
+    public Map<String, CollectionOperationStatus> insertLopBulk(List<String> keyList, Object element, CollectionAttributes attributesForCreate) {
+        Future<Map<String, CollectionOperationStatus>> future = null;
+        future = arcusClient.asyncLopInsertBulk(keyList, 0, element, attributesForCreate);
+        Map<String, CollectionOperationStatus> rv = null;
+        try {
+            rv = future.get(1000L, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            throw new IllegalStateException();
+        }
+        return rv;
+    }
+
+    public List<Object> getLop(String key, int index) {
+        Future<List<Object>> future = null;
+        future = arcusClient.asyncLopGet(key, index, false, false);
+        List<Object> rv = null;
+        try {
+            rv = future.get(1000L, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            throw new IllegalStateException();
+        }
+        return rv;
+    }
 }
--- src/test/java/ArcusLopTest.java
+++ src/test/java/ArcusLopTest.java
@@ -1,9 +1,65 @@
+import net.spy.memcached.collection.CollectionAttributes;
+import net.spy.memcached.ops.CollectionOperationStatus;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ArcusLopTest {
+    private ArcusLop arcusClient;
+    private final String TEST_VAL = "Brido";
+    private final String TEST_KEY = "TEST:KEY";
+
+    private final String TEST_PREFIX = "TEST";
+
+
+    @BeforeEach
+    public void set() {
+        arcusClient = new ArcusLop("127.0.0.1:2191", "brido");
+    }
+
+    @AfterEach
+    public void clear() {
+        boolean b = arcusClient.deleteAll(TEST_PREFIX);
+    }
+
+    @Test
+    @DisplayName("LOP 일괄 삽입 - 하나의 key, 여러개 요소")
+    void pipedInsertionLop() {
+
+        CollectionAttributes attributesForCreate = new CollectionAttributes();
+
+        List<Object> elements = new ArrayList<>();
+        for (int i = 0; i <= 500; i++) {
+            elements.add(TEST_VAL + i);
+        }
+        Map<Integer, CollectionOperationStatus> rv = arcusClient.insertLopPipedBulk(TEST_KEY, elements, attributesForCreate);
+    }
+
+    @Test
+    @DisplayName("LOP 일괄 삽입 - 여러개의 key, 하나의 요소")
+    void insertionBulkLop() {
+        CollectionAttributes attributesForCreate = new CollectionAttributes();
+
+        List<String> keyList = new ArrayList<>();
+        for (int i = 0; i <= 500; i++) {
+            keyList.add(TEST_PREFIX + i);
+        }
+
+        Map<String, CollectionOperationStatus> rv = arcusClient.insertLopBulk(keyList, TEST_VAL, attributesForCreate);
+
+        //then
+        Assertions.assertThat(rv.size()).isEqualTo(0);
+        for (int i = 0; i <= 500; i++) {
+            Assertions.assertThat(arcusClient.getLop(TEST_PREFIX + i, 0).get(0)).isEqualTo(TEST_VAL);
+        }
+
+    }
 }
