75be32f280fd0fb59f354a6c5497447a97d37d68
brido4125 <hcs4125@gmail.com>
2023-06-13 19:55:48 +0900

    update:FutureTask 테스트 코드

--- src/main/java/ArcusKeyValue.java
+++ src/main/java/ArcusKeyValue.java
@@ -1,8 +1,10 @@
+import net.spy.memcached.internal.GetFuture;
 import net.spy.memcached.internal.OperationFuture;
 import net.spy.memcached.ops.StatusCode;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class ArcusKeyValue extends ArcusInfo{
     public ArcusKeyValue(String arcusAdmin, String serviceCode) {
---
+++ src/test/java/isuue/futuretask/FutureTaskTest.java
@@ -0,0 +1,52 @@
+package isuue.futuretask;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class FutureTaskTest {
+
+  @Test
+  void test() throws ExecutionException, InterruptedException {
+    ExecutorService executorService = Executors.newFixedThreadPool(1);
+    Task<String> task = new Task<>(() -> {
+      System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
+      System.out.println("callable");
+      return "callable";
+    });
+    executorService.execute(task);
+    String s = task.get();
+    System.out.println("s = " + s);
+  }
+  private static class Task<T> extends FutureTask<T> {
+    private final AtomicBoolean isRunning = new AtomicBoolean(false);
+
+    public Task(Callable<T> callable) {
+      super(callable);
+    }
+
+    @Override
+    public T get() throws InterruptedException, ExecutionException {
+      this.run();
+      return super.get();
+    }
+
+    @Override
+    public T get(long timeout, TimeUnit unit) throws InterruptedException,
+            ExecutionException, TimeoutException {
+      this.run();
+      return super.get(timeout, unit);
+    }
+
+    @Override
+    public void run() {
+      System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
+      System.out.println("run1");
+      if (this.isRunning.compareAndSet(false, true)) {
+        System.out.println("run2");
+        super.run();
+      }
+    }
+  }
+}
