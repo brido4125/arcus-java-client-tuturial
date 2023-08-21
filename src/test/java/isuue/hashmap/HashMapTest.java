1a7efbce205874528714a08349fe868955982011
brido4125 <hcs4125@gmail.com>
2023-08-21 17:32:37 +0900

    update

--- src/test/java/isuue/hashmap/HashMapTest.java
+++ src/test/java/isuue/hashmap/HashMapTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 @Disabled
 public class HashMapTest {
@@ -21,6 +23,26 @@ public class HashMapTest {
       map.put(String.valueOf(i), "value" + i);
       int hash = (h = Integer.toString(i).hashCode()) ^ (h >>> 16);
       System.out.println("hash = " + hash);
+    }
+    System.out.println("map = " + map);
+  }
+
+  @Test
+  @DisplayName("Map에 동일한 Key, 다른 value put -> 가장 최근에 넣은 값으로 대체됨")
+  void sameKeyTest() {
+    Map<String, Integer> map = new HashMap<>();
+    for (int i = 0; i < 100000000; i++) {
+      map.put("hello", i);
+    }
+    System.out.println("map = " + map);
+  }
+
+  @Test
+  @DisplayName("Map에 동일한 Key, 다른 value put -> 가장 최근에 넣은 값으로 대체됨")
+  void sameKeyTest2() {
+    Map<String, Integer> map = new TreeMap<>();
+    for (int i = 0; i < 1000; i++) {
+      map.put("hello", i);
     }
     System.out.println("map = " + map);
   }
